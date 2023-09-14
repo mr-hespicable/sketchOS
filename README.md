71b6b1b492d64f25ffe23262fdf2ae83a0ffbd57
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-09-14 19:20:48 +0100

    started stuff

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 operating system built in rust
 
 this code is licensed under the 
+
+### devlog
+the devlog format is as follows:
+HH-MM-SS_DD-MM-YY
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn main() {
+//call on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }

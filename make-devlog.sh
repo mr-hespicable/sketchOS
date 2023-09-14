99291ee6df15ab7485e38f13ceacfa83d03de566
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-09-14 21:12:05 +0100

    devlog working

--- make-devlog.sh
+++ make-devlog.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Default values
-create_only=false
+create_only=true
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    -open)
-      create_only=true
+    --open)
+      create_only=false
       shift
       ;;
     *)
@@ -18,13 +18,13 @@ done
 
 mkdir -p "$(dirname "$0")/devlog"
 
-filename="devlog-$(date +%H:%M:%S-%d.%m.%y).txt"
+filename="devlog-$(date +%H.%M.%S-%d:%m:%y).txt"
 
 fullpath="$(dirname "$0")/devlog/$filename"
 
 touch "$fullpath"
 echo "created $filename at $fullpath"
 
-if [ "$create_only" = false" ]; then
-  nvim "$fullpath"
+if [ "$create_only" = false ]; then
+  nvim $fullpath
 fi

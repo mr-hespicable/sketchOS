357f9f014410d9b890c847992194c8ed866bbdfb
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-10-09 14:50:58 +0100

    added bootloader dependency

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
 //don't mangle this function's name (basically, don't fuck it up)
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     //_start() signifies that this is the entry point
+
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 

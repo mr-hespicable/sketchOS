3a01809c34d660ed89b7c5a4f0bb86d021079685
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-10-10 13:48:13 +0100

    test framework - initial configuration

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,40 @@
 #![no_std]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
+mod serial;
 mod vga_buffer;
+use core::panic::PanicInfo;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10, //2 is success
+    Failed = 0x11,  //3 is failure
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 //don't mangle this function's name (basically, don' fuck it up)
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("some panic msg");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
 //call this on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{_info}");
@@ -22,9 +43,28 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", _info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial_assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+    loop {}
 }

c668d459b805f47e1930ed673e4534eebe1ff8e1
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-10-10 11:38:20 +0100

    panic working

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,14 @@ mod vga_buffer;
 //don't mangle this function's name (basically, don' fuck it up)
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    //_start() signifies that this is the entry point
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("ello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some nums: {} {}", 42, 1.337).unwrap();
-
+    println!("Hello World{}", "!");
+    panic!("some panic msg");
     loop {}
 }
 
 //call this on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{_info}");
     loop {}
 }

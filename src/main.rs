3d5d763552fa795072a6f6faa1172f704c119c7a
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-10-10 11:54:17 +0100

    cargo test succeeds

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_main]
 #![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
@@ -17,4 +19,12 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{_info}");
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }

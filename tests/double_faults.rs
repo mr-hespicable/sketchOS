4487c399df4f9a6d896be6a06445ac794f29ee64
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-10-14 15:07:00 -0700

    it seems that when double_fault.rs test runs, only the last test is shown. will have to investigate further

--- tests/double_faults.rs
+++ tests/double_faults.rs
@@ -1,50 +1,24 @@
-#![no_std]
 #![no_main]
-#![feature(abi_x86_interrupt)]
+#![no_std]
 #![feature(custom_test_frameworks)]
 #![test_runner(sketch_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use lazy_static::lazy_static;
-use sketch_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use sketch_os::{exit_qemu, serial_println, QemuExitCode};
 
 static mut SHOULD_FAIL: bool = false;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    sketch_os::gdt::init();
-
+    sketch_os::init();
     test_main();
-
-    panic!("Execution continued after stack overflow");
+    #[allow(clippy::empty_loop)]
+    loop {}
 }
 
-//tests
-#[test_case]
-fn test_stack_overflow() {
-    serial_println!("testing stack");
-    stack_overflow();
-}
-
-#[allow(unconditional_recursion)]
-fn stack_overflow() {
-    stack_overflow();
-    volatile::Volatile::new(0).read(); // prevent tail recursion optimizations
-}
-
-#[test_case]
-fn test_double_fault() {
-    set_should_fail(true);
-    unsafe {
-        *(0xdeadbeef as *mut u8) = 42;
-    }
-}
-//end of tests
-
-//custom panic stuff
 fn should_fail() -> bool {
+    //hi
     unsafe { SHOULD_FAIL }
 }
 
@@ -69,3 +43,26 @@ fn panic_should_fail(_info: &PanicInfo) -> ! {
     exit_qemu(QemuExitCode::Success);
     loop {}
 }
+
+//tests
+
+#[test_case]
+fn stack_overflow() {
+    set_should_fail(true);
+    cause_stack_overflow();
+}
+
+#[allow(unconditional_recursion)]
+fn cause_stack_overflow() {
+    cause_stack_overflow();
+    volatile::Volatile::new(0).read();
+}
+
+#[test_case]
+fn double_fault() {
+    set_should_fail(true);
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    }
+}
+//end of tests

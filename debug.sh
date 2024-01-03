45a1ea31e819f0933f88e48a357d3b95eacc4a78
leon-archmbp <leonhmcqueen@gmail.com>
2024-01-03 13:02:03 +0000

    fixed debug stuff

--- debug.sh
+++ debug.sh
@@ -5,5 +5,5 @@ cargo run -- -s -S &
 sleep 2 #wait a bit for thing to run and stuff
 gdb -ex 'file target/x86_64-sketchOS/debug/sketch_os' -ex 'tar ext :1234' -ex 'tui enable' -ex 'b _start'
 
-PID = $(pgrep qemu)
+PID=$(pgrep qemu)
 kill $PID

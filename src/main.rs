3019eb60ab1447ea7670855e34e519a756642911
mr-hespicable@macbook-pro <85052021+mr-hespicable@users.noreply.github.com>
2024-02-26 07:20:00 +0000

    did more stuff (idk check git diff)
    p

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,12 @@
 #![reexport_test_harness_main = "test_main"]
 
 use bootloader::{entry_point, BootInfo};
-use core::panic::PanicInfo;
-use sketch_os::{draw_prompt, memory::active_level_4_table, println, MACHINE, USER};
+use core::{mem::transmute, panic::PanicInfo};
+use sketch_os::{
+    draw_prompt,
+    memory::{active_level_4_table, translate_addr},
+    println, MACHINE, USER,
+};
 use x86_64::{registers::control::Cr3, structures::paging::PageTable, VirtAddr};
 
 entry_point!(kernal_main);
@@ -15,25 +19,18 @@ fn kernal_main(boot_info: &'static BootInfo) -> ! {
     sketch_os::init(); //init idt
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 entry {}: {:?}", i, entry);
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
 
-            // get the physical address from the entry and convert it
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            // print non-empty entries of the level 3 table
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("    L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!(" {:?} -> {:?} ", virt, phys);
     }
     //draw_prompt!(&*USER.lock(), &*MACHINE.lock());
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,7 @@
-use x86_64::{structures::paging::{frame, Page, PageTable}, PhysAddr, VirtAddr};
+use x86_64::{
+    structures::paging::{frame, Page, PageTable},
+    PhysAddr, VirtAddr,
+};
 
 pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
@@ -12,25 +15,24 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     &mut *page_table_ptr //unsafe creation of page_table_ptr reference
 }
 
-pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) 
-    -> Option<PhysAddr> 
-{
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
     translate_addr_inner(addr, physical_memory_offset)
 }
 
-fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) 
-    -> Option<PhysAddr> 
-{
-    use x86_64::structures::paging::page_table::FrameError;
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
     use x86_64::registers::control::Cr3;
+    use x86_64::structures::paging::page_table::FrameError;
 
     let (level_4_table_frame, _) = Cr3::read(); //read lvl 4 reg 'CR3'
 
     let table_indexes = [
-        addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()
+        addr.p4_index(),
+        addr.p3_index(),
+        addr.p2_index(),
+        addr.p1_index(),
     ];
 
-    let mut frame = level_4_table_frame;
+    let mut frame = level_4_table_frame; //remember last physical frame
 
     //traverse the page tables
     for &index in &table_indexes {
@@ -43,11 +45,10 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr)
 
         frame = match entry.frame() {
             Ok(frame) => frame,
-            Err(FrameError::FrameNotPresent) => None,
-            Err(FrameError::HugeFrame) => panic!("huge pages not supported")
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
         }
-
-        //calculate & return physical address
-        Some(frame.start_address() + u64:;)
     }
+    //calculate & return physical address
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }

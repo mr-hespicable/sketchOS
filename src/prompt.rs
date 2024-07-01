0cbcf577ca35d6b825a8db0fa43de0be79236875
leon <leonhmcqueen@gmail.com>
2024-07-01 19:30:52 +0100

    rewrote using alloc crate
    removed stupid and idiotic lifetimes and just put the prompt into heap.
    also made proompt a proper struct, with good fields (may change later)

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,26 +1,38 @@
-//TODO: rewrite all of this using alloc crate stuff
+use core::str::FromStr;
+
+use alloc::{
+    format,
+    string::{String, ToString},
+};
+
+use crate::println;
+
 #[derive(Debug)]
-pub struct Prompt<'a>(&'a str);
+pub struct Prompt {
+    pub user: String,
+    pub machine: String,
+    pub prompt_text: String,
+    pub prompt_location: (usize, usize),
+}
 
-impl<'a> Prompt<'a> {
-    pub fn new(user: &'a str, machine: &'a str) -> [u8; 256] {
-        let mut prompt_array: [u8; 256] = [0; 256];
+impl Prompt {
+    pub fn new(user: String, machine: String) -> Prompt {
+        let prompt = format!("{}@{} ~>", user, machine).to_string(); // user@machine>>
 
-        let user_bytes: &[u8] = user.as_bytes(); //&[u8]
-        let machine_bytes: &[u8] = machine.as_bytes(); //&[u8]
-        let separator_bytes: &[u8] = "@".as_bytes();
-        let suffix_bytes: &[u8] = ">>".as_bytes();
-        //@ = 64 as bytes
-        //> = 62 as bytes
-        //goes `user@machine>>`
-        prompt_array[..user_bytes.len()].copy_from_slice(user_bytes); //add username
-        prompt_array[user_bytes.len()..user_bytes.len() + 1].copy_from_slice(separator_bytes); //add separator
-        prompt_array[user_bytes.len() + 1..user_bytes.len() + machine_bytes.len() + 1]
-            .copy_from_slice(machine_bytes); //add machine
-        prompt_array[user_bytes.len() + machine_bytes.len() + 1
-            ..user_bytes.len() + machine_bytes.len() + suffix_bytes.len() + 1]
-            .copy_from_slice(suffix_bytes); //add suffix
-
-        prompt_array //return prompt as [u8; 256]
+        return Prompt {
+            user,
+            machine,
+            prompt_text: prompt,
+            prompt_location: (0, 0),
+        };
     }
+}
+
+fn _main() {
+    let prompt = Prompt::new(
+        String::from_str("user").unwrap(),
+        String::from_str("machine").unwrap(),
+    );
+
+    println!("{:?}", prompt);
 }

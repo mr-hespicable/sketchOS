b69a7ed6a61aa2e84587818b964961f88fd2bf95
leon <leonhmcqueen@gmail.com>
2024-07-02 13:26:11 +0100

    removed main fn (unused)

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,11 +1,7 @@
-use core::str::FromStr;
-
 use alloc::{
     format,
     string::{String, ToString},
 };
-
-use crate::println;
 
 #[derive(Debug)]
 pub struct Prompt {
@@ -26,13 +22,4 @@ impl Prompt {
             prompt_location: (0, 0),
         }
     }
-}
-
-fn _main() {
-    let prompt = Prompt::new(
-        String::from_str("user").unwrap(),
-        String::from_str("machine").unwrap(),
-    );
-
-    println!("{:?}", prompt);
 }

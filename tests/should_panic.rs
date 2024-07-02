dca106317b89e6f3f2820536e51281fd09325942
leon <leonhmcqueen@gmail.com>
2024-07-02 09:33:39 +0100

    made should panic test not freak out

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -5,21 +5,21 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use sketch_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
+use sketch_os::{exit_qemu, hlt_loop, serial_print, serial_println, QemuExitCode};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     should_fail();
     serial_println!("[test did not panic]");
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop()
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
-    loop {}
+    hlt_loop()
 }
 
 fn should_fail() {

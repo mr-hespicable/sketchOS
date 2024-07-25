bf8bdc1a45b740af344d45d0739f21ae39f9f659
leon <leonhmcqueen@gmail.com>
2024-07-25 01:53:17 +0100

    moved process_command.rs into text_buffer.rs

--- src/text_buffer.rs
+++ src/text_buffer.rs
@@ -1,6 +1,8 @@
 use alloc::vec::Vec;
 use core::{fmt, str};
 
+use crate::process_commmand;
+
 #[derive(Default)]
 pub struct TextBuffer {
     information: Vec<u8>,
@@ -22,6 +24,7 @@ impl TextBuffer {
 
     /// Pushes a byte into the buffer
     pub fn append_byte(&mut self, byte: u8) {
+        if byte == b'\n' {}
         self.push(byte);
     }
 
@@ -43,5 +46,38 @@ impl TextBuffer {
 impl fmt::Display for TextBuffer {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", &self.information)
+    }
+}
+
+#[derive(Default)]
+pub struct CommandResult {
+    data_bytes: Vec<u8>,
+}
+
+impl fmt::Display for CommandResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", &self.data_bytes)
+    }
+}
+
+trait ProcessCommand {
+    fn process_command(&self, command: String) -> Option<CommandResult>;
+}
+
+impl ProcessCommand for TextBuffer {
+    fn process_command(&self, command: String) -> Option<CommandResult> {
+        if self.is_empty() {
+            return None;
+        }
+
+        if command.starts_with("echo ") {
+            let body: &str = command.splitn(2, " ").collect::<Vec<_>>()[1];
+
+            Some(CommandResult {
+                data_bytes: body.as_bytes().to_vec(),
+            })
+        } else {
+            None
+        }
     }
 }
